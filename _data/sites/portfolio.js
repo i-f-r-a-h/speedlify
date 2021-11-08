271af830ad594c5044e7e4e49d3b1e9409ca121b
Ifrah <90143819+i-f-r-a-h@users.noreply.github.com>
2021-11-08 11:00:35 +0000

    Update and rename eleventy-starters.js to portfolio.js

---
+++ _data/sites/portfolio.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  name: 'ifrahabdi.netlify.app',
+  description: "Ifrah's Portfolio 2021",
+  urls: ['https://www.ifrahabdi.netlify.app/', 'https://ifrahabdi.netlify.app/about.html', 'https://www.ifrahabdi.netlify.app/contact.html', 'https://ifrahabdi.netlify.app/blog.html', 'https://ifrahabdi.netlify.app/index.html'],
+  skip: false,
+  options: {
+    frequency: 1,
+    freshChrome: 'site',
+  },
+};
