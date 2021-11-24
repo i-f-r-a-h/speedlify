c45c193b8a42e5e6c31e9726daa700325ff30383
Ifrah <90143819+i-f-r-a-h@users.noreply.github.com>
2021-11-24 16:39:16 +0000

    Update portfolio.js

--- _data/sites/portfolio.js
+++ _data/sites/portfolio.js
@@ -2,7 +2,7 @@
 module.exports = {
   name: 'ifrahabdi.netlify.app',
   description: "Ifrah's Portfolio 2021",
-  urls: ['https://www.ifrahabdi.netlify.app/index.html', 'https://ifrahabdi.netlify.app/about.html', 'https://www.ifrahabdi.netlify.app/contact.html', 'https://ifrahabdi.netlify.app/blog.html'],
+  urls: ['https://ifrahabdi.netlify.app/about.html', 'https://www.ifrahabdi.netlify.app/contact.html', 'https://ifrahabdi.netlify.app/blog.html'],
   skip: false,
   options: {
     frequency: 1,
