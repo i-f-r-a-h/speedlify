0c3c39f5128d10d9797ea7ed2e978743e5cc8a34
Ifrah <90143819+i-f-r-a-h@users.noreply.github.com>
2021-12-08 19:57:20 +0000

    Update portfolio.js

--- _data/sites/portfolio.js
+++ _data/sites/portfolio.js
@@ -1,11 +1,10 @@
 
 module.exports = {
-	name: "Sample", // optional, falls back to object key
-	description: "The default sites that get tested",
-	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
-	},
-	urls: [
-		"https://ifrahabdi.netlify.app/"
-	]
+  name: 'ifrahabdi.netlify.app',
+  description: "Ifrah's Portfolio 2021",
+  urls: ['https://www.ifrahabdi.netlify.app/', 'https://ifrahabdi.netlify.app/about.html', 'https://www.ifrahabdi.netlify.app/contact.html', 'https://ifrahabdi.netlify.app/blog.html'],
+  options: {
+    frequency: 1,
+    freshChrome: 'site',
+  },
 };
