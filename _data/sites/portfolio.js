664db7a3f81cf45f982bff9bc5fa80df679f603f
Ifrah <90143819+i-f-r-a-h@users.noreply.github.com>
2021-12-08 19:50:22 +0000

    Update portfolio.js

--- _data/sites/portfolio.js
+++ _data/sites/portfolio.js
@@ -1,11 +1,19 @@
 
 module.exports = {
-  name: 'ifrahabdi.netlify.app',
-  description: "Ifrah's Portfolio 2021",
-  urls: ['https://ifrahabdi.netlify.app'],
-  skip: false,
-  options: {
-    frequency: 1,
-    freshChrome: 'site',
-  },
+	description: "Ifrah's portfolio",
+	options: {
+		frequency: 60 * 11 + 30, // 11h, 30m
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "site",
+	},
+	urls: [
+		"https://ifrahabdi.netlify.app/",
+		"https://ifrahabdi.netlify.app/about.html",
+		"https://ifrahabdi.netlify.app/blog.html",
+		"https://ifrahabdi.netlify.app/contact.html",
+		"https://webhint.io/"
+	]
 };
